9247d86c15ed243546d94d9dca77c18e1c7a752a
Lucas Silva e Pereira <lucas4162007@gmail.com>
2023-04-28 15:00:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calculadora-v.1.0',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
